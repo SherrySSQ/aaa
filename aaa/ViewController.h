cb7e7378b67ec9b74d8302bb5649226483e7701f
孙少青 <sunshaoqing@sunshaoqingdeMacBook-Pro.local>
2018-07-28 15:14:31 +0800

    Initial Commit

---
+++ aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaa
+//
+//  Created by 孙少青 on 2018/7/28.
+//  Copyright © 2018年 孙少青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aaa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  aaa
+//
+//  Created by 孙少青 on 2018/7/28.
+//  Copyright © 2018年 孙少青. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
